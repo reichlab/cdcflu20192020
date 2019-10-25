14c8c1fa164908d6a5d382fdc76010215590ec1f
Nicholas Reich <nick@schoolph.umass.edu>
2019-10-25 12:05:26 +0200

    first pass at KDE fits and predictions for 2019/2020

--- inst/estimation/region-kde/create-current-season-kde-entry-files.R
+++ inst/estimation/region-kde/create-current-season-kde-entry-files.R
@@ -1,7 +1,8 @@
-## make a set of prospective prediction files for KDE/GAM model for 2017/2018 season
+## make a set of prospective prediction files for KDE/GAM model
 ## Nicholas Reich
 ## October 2017: created
 ## October 2018: updated
+## October 2019: updated and ported to new package.
 
 library(cdcfluforecasts)
 library(doMC)
@@ -13,10 +14,10 @@ data(flu_data)
 
 n_sims <- 100000
 
-## for 2018-2019 season, 
-## first predictions due on 10/29/2018 (EW44 == SW14)  MMWRweek("2018-10-29")
-## using data posted on 10/26/2016 that includes up to EW42 == SW12
-## last predictions due on 5/13/2017 (EW20 == SW 42) MMWRweek("2019-05-13")
+## for 2019-2020 season, 
+## first predictions due on 10/28/2019 (EW44 == SW14)  MMWRweek("2019-10-28")
+## using data posted on 10/25/2019 that includes up to EW42 == SW12
+## last predictions due on 5/11/2020 (EW20 == SW 42) MMWRweek("2020-05-11")
 ## last predictions use data through EW18 == SW40
 ## first_analysis_time_season_week could be set to 15, but padding at front end
 
@@ -26,7 +27,7 @@ fit_path <- "inst/estimation/region-kde/fits/"
 
 registerDoMC(3)
 
-## fit 2018/2019 models
+## fit 2019/2020 models
 foreach(reg=region_strings) %dopar% {
     
   ## fit models on training seasons, using only prospective data, not LOSO
@@ -41,11 +42,18 @@ foreach(reg=region_strings) %dopar% {
     first_fit_week = 20, 
     path = fit_path)
 }
+## will receive the following warning when a region is fit and there exists a season with no onset:
+
+## Warning message:
+## In cbind(unique(data$season), as.numeric(unlist(observed_seasonal_quantities_by_season[,  :
+##     NAs introduced by coercion
+
+
 
 ## make entry files
 foreach(season_week = season_weeks) %dopar% {
   ## season_week <- season_weeks[1]
-  make_one_kde_prediction_file(save_path = "inst/submissions/region-kde/",
+  make_one_kde_prediction_file(save_path = "inst/prospective-predictions/region-kde/",
     fits_path = fit_path,
     season = this_season,
     season_week = season_week,
